7195e8d6c2a16641f178a891a291d865a862c104
Johan Svanborg <johan.svanborg@mogul.com>
2016-10-06 13:16:41 +0200

    Initial commit, adding solution file

---
+++ TestGIT/MyConsoleApplication.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApplication.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Test");
+        }
+    }
+}
