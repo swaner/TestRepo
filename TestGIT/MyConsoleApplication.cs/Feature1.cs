61859e99b0871032072ff77c6d4d8a16fa8222e2
Johan Svanborg <johan.svanborg@mogul.com>
2016-11-24 12:42:44 +0100

    New cool feature

---
+++ TestGIT/MyConsoleApplication.cs/Feature1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApplication.cs
+{
+    public class Feature1
+    {
+        // Important feature
+
+        //
+    }
+}
